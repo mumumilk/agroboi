f8f6f2075453053d0911298427117dad3e7459c2
LuisMiara <luisgustavomiara@gmail.com>
2017-10-14 10:37:39 -0300

    tela login
    Adição da tela de login e cadastro

---
+++ src/pages/cadastro/cadastro.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Cadastro } from './cadastro';
+
+@NgModule({
+  declarations: [
+    Cadastro,
+  ],
+  imports: [
+    IonicPageModule.forChild(Cadastro),
+  ],
+})
+export class CadastroModule {}
---
+++ src/pages/cadastro/cadastro.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the Cadastro page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-cadastro',
+  templateUrl: 'cadastro.html',
+})
+export class Cadastro {
+  public documento= '';
+  public nome= '';
+  public email= '';
+  public usuario= '';
+  public senha= '';
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Cadastro');
+  }
+
+}

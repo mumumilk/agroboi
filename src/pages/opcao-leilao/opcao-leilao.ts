3bb3cf2fb70c7ffa11839ac1fbe981ab2bfd6011
LuisMiara <luisgustavomiara@gmail.com>
2017-10-15 10:20:52 -0200

    propagandas
    propagandas

--- src/pages/opcao-leilao/opcao-leilao.ts
+++ src/pages/opcao-leilao/opcao-leilao.ts
@@ -14,12 +14,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'opcao-leilao.html',
 })
 export class OpcaoLeilaoPage {
-
+  public testCheckboxResultRaca;
+  public testCheckboxResultSangue;
+  public testCheckboxResultCategoria;
+  public testCheckboxOpen;
+  public structure: any = { lower: 1000, upper: 10000 };
+  public structureIdade: any = { lower: 18, upper: 30 };
+  public event = {
+    Inicio: '2017-10-15',
+    Fim: '2017-10-30',
+    timeStarts: '07:43',
+    timeEnds: '1990-02-20'
+  }
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OpcaoLeilaoPage');
   }
+
+
 
 }
